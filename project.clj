fc59387307ef83edc9a038e3bdfe016b5c2091be
Evgeniy Kochetkov <ekochetkov.work@gmail.com>
2022-06-17 16:03:31 +0300

    Project configs

---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject hs-patients-crud "hs-patients-crud"
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [prismatic/schema "1.2.1"]
+                 [org.clojure/tools.logging "1.2.4"]
+                 [http-kit "2.1.18"]
+                 [compojure "1.3.4"]
+                 [hiccup "1.0.5"]
+                 [migratus/migratus "1.3.5"]
+		 [org.clojure/java.jdbc "0.6.1"]
+		 [org.postgresql/postgresql "9.4-1201-jdbc41"]
+                 [org.clojure/data.json "2.4.0"]
+                 [stylefruits/gniazdo "1.2.1"]]
+
+  :plugins [ [migratus-lein "0.7.3"] ]
+
+  :main backend.core
+  :aot :all)
