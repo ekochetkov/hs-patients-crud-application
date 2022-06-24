10c23b842efd9cde4dfd03a16a912fffee9a3d1e
Evgeniy Kochetkov <ekochetkov.work@gmail.com>
2022-06-24 10:12:25 +0300

    Noise reduced

--- src/frontend/patients.cljs
+++ src/frontend/patients.cljs
@@ -1,14 +1,8 @@
 (ns frontend.patients
   (:require
-   ["rc-easyui" :refer [DataGrid GridColumn Layout LayoutPanel Tree LinkButton Dialog Form TextBox Label DateBox SearchBox MaskedBox ComboBox FormField]]
+   ["rc-easyui" :refer [DataGrid GridColumn LinkButton Dialog Form TextBox DateBox SearchBox MaskedBox ComboBox FormField]]
    [reagent.core :as r]
-   [reagent.dom :as rdom]   
-   [re-frame.core :as rf]
-   [goog.string :as gstring]
-
-   [frontend.comm :as comm]
-   
-   [websocket-fx.core :as wfx]))
+   [re-frame.core :as rf]))
 
 (def db {:patients {:data []
                     :data-filtered []
@@ -115,11 +109,7 @@
                                  :onClick #(rf/dispatch [:patients/patients-reload]) } "Reload"]
                  [:> SearchBox {:style {:float "right" :margin "5px" :width "350px"}
                                 :value (:text-like @filters)
-                                :onChange #(rf/dispatch [:patients/update-data-filters [:text-like %]])}]
-                 ]
-
-                )
-  ))
+                                :onChange #(rf/dispatch [:patients/update-data-filters [:text-like %]])}]])))
 
 (defn column-render-text-like [value text-like]
   (if (and value text-like)
@@ -137,39 +127,28 @@
                  :style {:height "100%"}
                  :selectionMode "single"
                  :toolbar datagrid-toolbar}
-    [:> GridColumn {:width "30px" :align "center" :field "#" :title "#"
-                    }]
+    [:> GridColumn {:width "30px" :align "center" :field "#" :title "#"}]
     [:> GridColumn {:width "250px" :title "Patient name"
                      :render #(-> % .-row (aget "resource") (aget "patient_name")
                                  (column-render-text-like (:text-like @filters)))}]
-    
     [:> GridColumn {:width "90px" :title "Birth date"
                     :render #(-> % .-row (aget "resource") (aget "birth_date")
-                                 (column-render-text-like (:text-like @filters)))
-                    }]
-
+                                 (column-render-text-like (:text-like @filters)))}]
     [:> GridColumn {:width "70px" :title "Gender"
                     :render #(-> % .-row (aget "resource") (aget "gender")
-                                 (column-render-text-like (:text-like @filters)))
-                    }]
+                                 (column-render-text-like (:text-like @filters)))}]
     [:> GridColumn {:width "50%" :title "Address"
                     :render #(-> % .-row (aget "resource") (aget "address")
-                                 (column-render-text-like (:text-like @filters)))
-
-                    }]
+                                 (column-render-text-like (:text-like @filters)))}]
     [:> GridColumn {:width "50%"  :title "Policy number"
                     :render #(-> % .-row (aget "resource") (aget "policy_number")
-                                 (column-render-text-like (:text-like @filters)))
-
-                    }]
-     ]]))
+                                 (column-render-text-like (:text-like @filters)))}]]]))
 
 (defn dialog-create []
   (let [show-dialog (rf/subscribe [:patients/show-dialog])
         button-create-disabled (rf/subscribe [:patients/form-data-invalid])
         form-data (rf/subscribe [:patients/form-data])
-        label-width "130px"
-        ]
+        label-width "130px"]
         [:> Dialog
          {:closed (not= @show-dialog :create)
           :onClose #(rf/dispatch [:patients/show-dialog nil])
@@ -186,71 +165,39 @@
              "policy_number" {"required" true
                               "all-numbers" {"validator" #(re-find (js/RegExp "[\\d]{4}\\ [\\d]{4}\\ [\\d]{4}\\ [\\d]{4}") %)
                                              "message" "Need all digits"}}}
-                                              
-                                              
      :errorType "tooltip"
      :className "f-full"
      :model @form-data
      :onChange (fn [f v]
                  (when (not= f "birth_date")
                    (rf/dispatch [:patients/update-form-data [f v]])))}
-;    [:div {:style {:margin-bottom "10px"}}
-;     [:> Label {:htmlfor "inp_patient_name" :style {:width label-width}} "Patient name:"]
-;     [:> TextBox {:name "patient_name"
-;                  :inputId "inp_patient_name" :style {:width "400px"} :iconCls "icon-man"}]
-                                        ;     ]
     [:> FormField {:name "patient_name"
                    :style {:margin-bottom "10px"} :labelAlign "right" :labelWidth label-width :label "Patient name: "}
-     [:> TextBox {
-                    :inputId "inp_patient_name" :style {:width "400px"} :iconCls "icon-man"}]    
-     ]
+     [:> TextBox {:inputId "inp_patient_name" :style {:width "400px"} :iconCls "icon-man"}]]
     
-
-;    [:div {:style {:margin-bottom "10px"}}
-                                        ;     [:> Label {:htmlfor "inp_birth_date" :style {:width label-width}} "Birth date"]
     [:> FormField {:name "birth_date"
                    :style {:margin-bottom "10px"} :labelAlign "right" :labelWidth label-width :label "Birth date: "}
      [:> DateBox {:inputId "inp_birth_date" :format "yyyy-MM-dd"
                   :onChange #(rf/dispatch [:patients/update-form-data ["birth_date" (.getTime %)]])
-                  :style {:width "200px"}}]
-]
-     
-     #_[:div {:class "error"} "some error"]
+                  :style {:width "200px"}}]]
     
- ;   [:div {:style {:margin-bottom "10px"}}
-                                        ;     [:> Label {:htmlfor "inp_gender" :style {:width label-width}} "Gender:"]
-  [:> FormField {:name "gender"
+    [:> FormField {:name "gender"
                    :style {:margin-bottom "10px"} :labelAlign "right" :labelWidth label-width :label "Gender: "}
      [:> ComboBox {:data [{:value "male" :text "Male"}{:value "female" :text "Female"}]
-                   :inputId="inp_gender" :name "gender" :style {:width "200px"}}]
-     #_[:div {:class "error"} "some error"]]
+                   :inputId="inp_gender" :name "gender" :style {:width "200px"}}]]
 
-;    [:div {:style {:margin-bottom "10px"}}
-                                        ;     [:> Label {:htmlfor "inp_address" :style {:width label-width}} "Address:"]
-  [:> FormField {:name "address"
+    [:> FormField {:name "address"
                    :style {:margin-bottom "10px"} :labelAlign "right" :labelWidth label-width :label "Address: "}    
-     [:> TextBox {:inputId="inp_address" :style {:width "400px" :height "70px"} :multiline true}]
-     #_[:div {:class "error"} "some error"]]
-
-;    [:div {:style {:margin-bottom "10px"}}
-                                        ;     [:> Label {:htmlfor "inp_policy_number" :style {:width label-width}} "Policy number:"]
-      [:> FormField {:name "policy_number"
+     [:> TextBox {:inputId="inp_address" :style {:width "400px" :height "70px"} :multiline true}]] 
+    
+    [:> FormField {:name "policy_number"
                    :style {:margin-bottom "10px"} :labelAlign "right" :labelWidth label-width :label "Policy number: "}
-     [:> MaskedBox {:mask "9999 9999 9999 9999" :inputId="inp_policy_number"  :style {:width "155px"}}]
-     #_[:div {:class "error"} "some error"]]
-      
-    ]]
-
-   ;[:p "xx" @button-create-disabled]
-         
+     [:> MaskedBox {:mask "9999 9999 9999 9999" :inputId="inp_policy_number"  :style {:width "155px"}}]]]]
+   
    [:div {:className "dialog-button"}
-     [:> LinkButton
-      {:disabled @button-create-disabled
-       :onClick #(rf/dispatch [:patients/send-event-create])
-        :style {:width "80px"}} "Create"]
-    ]
-         
-     ]))
+     [:> LinkButton {:disabled @button-create-disabled
+                     :onClick #(rf/dispatch [:patients/send-event-create])
+                     :style {:width "80px"}} "Create"]]]))
 
 (defn ui []
   [:div [datagrid] [dialog-create]])
