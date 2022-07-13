828ee02821a877d3ca63d50748060fe92da8a787
Evgeniy Kochetkov <ekochetkov.work@gmail.com>
2022-07-13 14:29:19 +0000

    Check envs dependent on jobs need other mechanism check on events

--- src/backend/context.clj
+++ src/backend/context.clj
@@ -4,5 +4,7 @@
           :ws-url (System/getenv "WS_URL")
           :port (System/getenv "PORT")})
 
-(when (nil? (:db-spec ctx))
-  (throw (Exception. "Need env var 'DATABASE_URL'")))
+;; TODO: Check envs dependent on jobs
+;;       need other mechanism check on events
+;;(when (nil? (:db-spec ctx))
+;;  (throw (Exception. "Need env var 'DATABASE_URL'")))
