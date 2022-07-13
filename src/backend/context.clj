19fdaf3a04fcae5b9257f30c33bdfecaeb184e25
Evgeniy Kochetkov <ekochetkov.work@gmail.com>
2022-07-13 13:50:33 +0000

    Check env WS_URL, PORT only on run server

--- src/backend/context.clj
+++ src/backend/context.clj
@@ -6,9 +6,3 @@
 
 (when (nil? (:db-spec ctx))
   (throw (Exception. "Need env var 'DATABASE_URL'")))
-
-(when (nil? (:ws-url ctx))
-  (throw (Exception. "Need env var 'WS_URL'")))
-
-(when (nil? (:port ctx))
-  (throw (Exception. "Need env var 'PORT'")))
--- src/backend/core.clj
+++ src/backend/core.clj
@@ -33,9 +33,15 @@
   (run-server #'app {:port port}))
 
 (defn -main []
-  (println "Entry main")
+
+  (when (nil? (:ws-url ctx))
+    (throw (Exception. "Need env var 'WS_URL'")))
+
+  (when (nil? (:port ctx))
+    (throw (Exception. "Need env var 'PORT'")))
+
   (reset! server-stop-func (run-server #'app {:port (Integer/parseInt (:port ctx))}))
-  (println "Run server on localhost at port: 9009"))
+  (println "Run server on localhost at port: " (:port ctx)))
 
 ;(@server-stop-func)
 ;(-main)
