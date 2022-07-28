9e395b503b800a271799631f0736b657dd26beda
Evgeniy Kochetkov <ekochetkov.work@gmail.com>
2022-07-28 17:42:25 +0000

    Added check env database-url

--- src/backend/context.clj
+++ src/backend/context.clj
@@ -4,5 +4,5 @@
           :ws-url (System/getenv "WS_URL")
           :port (System/getenv "PORT")})
 
-;(when (nil? (:db-spec ctx))
-;  (throw (Exception. "Need env var 'DATABASE_URL'")))
+(when (nil? (:db-spec ctx))
+  (throw (Exception. "Need env var 'DATABASE_URL'")))
