aa8541318ed6776026b3bcbb34af8350f52220e4
Evgeniy Kochetkov <ekochetkov.work@gmail.com>
2022-08-03 08:05:30 +0000

    Added env var DATABASE_URL_PARAMS for add extra param for connection

--- src/backend/context.clj
+++ src/backend/context.clj
@@ -1,5 +1,7 @@
 (ns backend.context)
 
-(def ctx {:db-spec (System/getenv "DATABASE_URL")
+(def ctx {:db-spec (str
+                    (System/getenv "DATABASE_URL")
+                    (System/getenv "DATABASE_URL_PARAMS"))
           :ws-url (System/getenv "WS_URL")
           :port (System/getenv "PORT")})
