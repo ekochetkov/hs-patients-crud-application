7fe3c13692311bb286f2f4f14016e378c684b572
Evgeniy Kochetkov <ekochetkov.work@gmail.com>
2022-08-09 16:06:18 +0300

    Delete layout reference

--- src/frontend/db.cljs
+++ src/frontend/db.cljs
@@ -2,7 +2,6 @@
   (:require
    [re-frame.core :as rf]
    [frontend.patients :as p]
-   [frontend.layout :as l]
    [re-frame.db]))
 
 (def locales {:en {:strings {:patinents.datagrid.column.patient-name "Patient name"}
@@ -15,9 +14,7 @@
 (def db {:locale-lang :ru
          :locale (:ru locales)
          
-         :. {:layout l/init-state
-             :patients p/init-state}
-         })
+         :. {:patients p/init-state}})
 
 (rf/reg-sub :locale-lang #(-> % :frontend :locale-lang))
 
